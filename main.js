06402e263e5684a60ed2c5a5a8e0cc8c08988a62
inkilu <gauthamgkm@gmail.com>
2020-11-13 00:05:24 +0530

    Various new features

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const Discord = require ('discord.js');
+const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
 
@@ -9,7 +9,7 @@ client.once('ready', () => {
 
 });
 
-client.login('');
+client.login('Nzc1NjkyMjE5OTg1OTUyODE4.X6qBrA.FP1UBnJnpfMh1GvDHJmVJc-o7eg');
 
 client.on('message', message => {
     if (message.content === 'mwona') {
@@ -17,7 +17,35 @@ client.on('message', message => {
     }
 });
 
-const theri = [
+let botID = '775692219985952818';
+client.on('message', message => {
+    if ((message.content.toLowerCase() === 'hi' || message.content.toLowerCase() === 'hello') && (message.member.id !== botID)) {
+        message.reply('Hello chellam umma veno??!? 😘😘');
+        console.log(message.member.id);
+    }
+});
+let emoji = [
+    '😘','🥰','💋',
+    '👋', '🤚', '🖐',
+    '✋', '🖖', '👌',
+    '✌️', '🤞', '🤟',
+    '🤘', '🤙', '🖕',
+    '👍', '👎', '✊',
+    '👊', '👏', '🙌',
+    '🤝', '🙏','🤣'
+
+];
+
+// React to every message in every channel with emoji
+client.on('message', message => {
+    if (message.content) {
+        let reaction = emoji[Math.floor(Math.random() * 24)];
+        message.react(reaction);
+    }
+});
+
+
+const bed = [
     'fuk u mwona',
     'fuck you',
     'fuk u',
@@ -26,8 +54,8 @@ const theri = [
 ];
 
 client.on('message', message => {
-    for (let i = 0; i <= theri.length; i++) {
-        if (message.content == theri[i]) {
+    for (let i = 0; i <= bed.length; i++) {
+        if (message.content == bed[i]) {
             message.reply('No bed werds mwona');
             break;
         }
@@ -45,12 +73,17 @@ client.on('message', message => {
 // Joining Voice Channel
 
 let connection;
-client.on('message', async message => {
+client.on('message', message => {
     if (message.content === 'join pls') {
-        if (message.member.voice.channel) {
-             connection = await message.member.voice.channel.join();
+
+        if (!message.member.voice.channelID) {
+            message.reply('Voice channelil keru mwonaaa');
+            return;
         }
+        message.member.voice.channel.join();
+
     }
 });
-/* const dispatcher = connection.play('https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3');
-dispatcher.on('error', console.error); */
+
+
+
